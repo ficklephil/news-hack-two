f7f7809b11f7b1a22a093ea1be9f96703eda2ae0
Marcos Carceles <marcos.carceles@gmail.com>
2014-05-01 19:36:29 +0100

    You can login!

---
+++ grails-app/domain/news/hack/two/Context.groovy
@@ -0,0 +1,26 @@
+package news.hack.two
+
+class Context {
+
+    //User user;
+
+    int happy
+    int sad
+    int optimistic
+    int pesimistic
+    int hopeful
+    int fearful
+    int amused
+    int unamused
+    int excited
+    int angry
+    int nostalgic
+    int flabbergasted
+
+    int total
+
+    //static belongsTo = [user:User]
+
+    static constraints = {
+    }
+}
