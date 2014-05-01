aedda650795c753033b54988d060db46f9a57d65
hosainnet <contact@hosain.net>
2014-05-01 18:40:44 +0100

    @ha removed bootstrapped stories

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,18 +5,6 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        def random = new Random();
-
-        (1..100).each {
-            def story = new Story(body: "Story ${it}");
-            story.publishDate = new Date();
-            Mood.values()*.toString().each { mood ->
-                int rating = random.nextInt(20);
-                story[mood] = rating
-                story.ratings += rating;
-            }
-            story.save()
-        }
     }
     def destroy = {
     }
