f76b87ff7af3121f0134d3b51329248a5a3f74ef
Marcos Carceles <marcos.carceles@gmail.com>
2014-05-01 16:34:09 +0100

    Serving stories based on context!

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,22 @@
+import news.hack.two.Mood
+import news.hack.two.Story
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def random = new Random();
+
+        (1..100).each {
+            def story = new Story(body: "Story ${it}");
+            story.publishDate = new Date();
+            Mood.values()*.toString().each { mood ->
+                int rating = random.nextInt(20);
+                story[mood] = rating
+                story.ratings += rating;
+            }
+            story.save()
+        }
     }
     def destroy = {
     }
